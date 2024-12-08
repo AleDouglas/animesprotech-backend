073ff74d8e5fb3780f8aa304f18f0622bd413fe5
Alexandre Douglas Leite <100922152+AleDouglas@users.noreply.github.com>
2024-12-08 12:57:56 -0300

    Adicionando configuração do JWT

--- AnimesProtech.Web/Program.cs
+++ AnimesProtech.Web/Program.cs
@@ -2,6 +2,9 @@ using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using AnimesProtech.Infrastructure.Data;
 using AnimesProtech.Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 Env.Load();
 
@@ -9,19 +12,44 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<LogService>();
+
+var secretKey = Env.GetString("SECRET_KEY") ?? throw new InvalidOperationException("SECRET_KEY não configurada no .env");
+var key = Encoding.ASCII.GetBytes(secretKey);
+builder.Services.AddAuthentication(options => {
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+
+
+
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()){
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
