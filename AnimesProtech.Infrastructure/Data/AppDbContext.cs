02b198b0f552e4ab2e347a8a68252cf3fac2d87d
Alexandre Douglas Leite <100922152+AleDouglas@users.noreply.github.com>
2024-12-08 10:44:27 -0300

    Criando AppDbContext

---
+++ AnimesProtech.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using AnimesProtech.Web.Domain.Entities;
+
+namespace AnimesProtech.Web.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Anime> Animes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Para mapeamento futuro
+        }
+    }
+}
