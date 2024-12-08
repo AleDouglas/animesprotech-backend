c35e08cec875301bfc483b13302922f24bc15a32
Alexandre Douglas Leite <100922152+AleDouglas@users.noreply.github.com>
2024-12-08 12:24:35 -0300

    Registrando serviço

--- AnimesProtech.Web/Program.cs
+++ AnimesProtech.Web/Program.cs
@@ -1,15 +1,15 @@
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using AnimesProtech.Infrastructure.Data;
+using AnimesProtech.Infrastructure.Services;
 
 Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddScoped<LogService>();
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
